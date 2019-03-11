7c4d136f259a62f891637c0e6cd43d2ffc77fd48
unknown <nitaiulianmarius@gmail.com>
2019-03-11 23:34:53 +0200

    added files

---
+++ trainer/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace trainer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.sunTxtBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(136, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sun:";
+            // 
+            // sunTxtBox
+            // 
+            this.sunTxtBox.Location = new System.Drawing.Point(4, 52);
+            this.sunTxtBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.sunTxtBox.Name = "sunTxtBox";
+            this.sunTxtBox.Size = new System.Drawing.Size(147, 29);
+            this.sunTxtBox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(159, 47);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 37);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Hack";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Status conectare";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.sunTxtBox);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(82, 130);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(290, 97);
+            this.panel1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(464, 351);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("Urban Brush Zone", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Plants vs Zombie, baTTLe4u trainer";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox sunTxtBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ trainer/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace trainer
+{
+    public partial class Form1 : Form
+    {
+        Thread guiThread;
+        Thread connectThread;
+        Memory memory;
+
+        string sunAddress = "0019926C";
+        int[] sunOffSet = { 0x5560 };
+        int sunValue;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            connectThread = new Thread(connect);
+            connectThread.IsBackground = true;
+            connectThread.Start();
+            guiThread = new Thread(gui);
+            guiThread.IsBackground = true;
+            guiThread.Start();
+        }
+
+        void gui()
+        {
+            while(true)
+            {
+                Thread.Sleep(100);
+                try
+                {
+                    this.Invoke(new MethodInvoker(delegate ()
+                    {
+                        if (memory != null)
+                        {
+                            if (memory.connected == true)
+                            {
+                                label2.Text = "Conectat la joc";
+                                panel1.Enabled = true;
+                            }
+                            else
+                            {
+                                label2.Text = "Nu gasesc jocul";
+                                panel1.Enabled = false;
+                            }
+
+                            sunValue = memory.PointerRead(sunAddress, sunOffSet);
+                            label1.Text = "Sun: " + sunValue;
+                        }
+                        else
+                        {
+                            label2.Text = "Nu gasesc jocul";
+                            panel1.Enabled = false;
+                        }
+                    }));
+                }
+                catch
+                {
+                    ;
+                }
+            }
+        }
+
+        void connect()
+        {
+            memory = new Memory("popcapgame1");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int newValue = int.Parse(sunTxtBox.Text);
+            memory.PointerWrite(sunAddress, BitConverter.GetBytes(newValue), sunOffSet);
+            sunTxtBox.Text = "";
+        }
+    }
+}
